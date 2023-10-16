27b61b9c4f94244bb149ab45ab4cb1450d38b3b5
anas-deriv <hanasrullah@regentmarkets.com>
2023-10-16 17:54:08 +0800

    empty commit

--- lib/src/config/presentation/views/bottom_navigation_bar_place_holder.dart
+++ lib/src/config/presentation/views/bottom_navigation_bar_place_holder.dart
@@ -1,3 +1,4 @@
+import 'package:bikecare/src/utils/constants/strings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -26,21 +27,21 @@ class _BottomNavigationBarPlaceHolderState
     extends State<BottomNavigationBarPlaceHolder> {
   final bottomNavigationItems = [
     BottomNavigationBarRoute(
-      initialLocation: PAGES.dashboard.screenPath,
-      icon: const Icon(Icons.dashboard_sharp),
-    ),
+        initialLocation: PAGES.dashboard.screenPath,
+        icon: const Icon(Icons.dashboard_sharp),
+        label: bottomNavigationBarItemsLabel[0]),
     BottomNavigationBarRoute(
-      initialLocation: PAGES.notification.screenPath,
-      icon: const Icon(Icons.notifications),
-    ),
+        initialLocation: PAGES.notification.screenPath,
+        icon: const Icon(Icons.notifications),
+        label: bottomNavigationBarItemsLabel[1]),
     BottomNavigationBarRoute(
-      initialLocation: PAGES.planning.screenPath,
-      icon: const Icon(Icons.edit_calendar),
-    ),
+        initialLocation: PAGES.planning.screenPath,
+        icon: const Icon(Icons.edit_calendar),
+        label: bottomNavigationBarItemsLabel[2]),
     BottomNavigationBarRoute(
-      initialLocation: PAGES.profile.screenPath,
-      icon: const Icon(Icons.manage_accounts),
-    ),
+        initialLocation: PAGES.profile.screenPath,
+        icon: const Icon(Icons.manage_accounts),
+        label: bottomNavigationBarItemsLabel[3]),
   ];
 
   @override
