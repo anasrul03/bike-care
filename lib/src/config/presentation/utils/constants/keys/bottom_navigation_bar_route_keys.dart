57cb7564d6e6824130f8b1789a79171c24a19653
anas-deriv <hanasrullah@regentmarkets.com>
2023-10-19 11:00:35 +0800

    adding theme with text family
    NotoSans tff file

---
+++ lib/src/config/presentation/utils/constants/keys/bottom_navigation_bar_route_keys.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Key dashboardBottomNavBarKey = Key('dashboardBottomNavBarKey');
+const Key notificationBottomNavBarKey = Key('notificationBottomNavBarKey');
+const Key planningBottomNavBarKey = Key('planningBottomNavBarKey');
+const Key profileBottomNavBarKey = Key('profileBottomNavBarKey');
